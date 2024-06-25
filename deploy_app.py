84b088bb4bd52369fbe6608718d44fd993b12f90
Penosh22 <142080192+Penosh22@users.noreply.github.com>
2024-06-25 14:17:16 +0530

    Update deploy_app.py

--- deploy_app.py
+++ deploy_app.py
@@ -13,27 +13,27 @@ if st.button("predict"):
     df = df.drop(df.tail(1).index)
     df.reset_index(inplace=True)
     data1 = {
-        'Date1': df.tail(2)['datetime'].iloc[0],
-        'Open1': df.tail(2)['open'].iloc[0],
-        'High1': df.tail(2)['high'].iloc[0],
-        'Low1': df.tail(2)['low'].iloc[0],
-        'Close1': df.tail(2)['close'].iloc[0]
+        'Date1': df.iloc[-2]['datetime'],
+        'Open1': df.iloc[-2]['open'],
+        'High1': df.iloc[-2]['high'],
+        'Low1': df.iloc[-2]['low'],
+        'Close1': df.iloc[-2]['close']
     }
         
         
     data2 = {
-        'Date2': df.tail(2)['datetime'].iloc[1],
-        'Open2': df.tail(2)['open'].iloc[1],
-        'High2': df.tail(2)['high'].iloc[1],
-        'Low2': df.tail(2)['low'].iloc[1],
-        'Close2': df.tail(2)['close'].iloc[1]
+        'Date2': df.iloc[-1]['datetime'],
+        'Open2': df.iloc[-1]['open'],
+        'High2': df.iloc[-1]['high'],
+        'Low2': df.iloc[-1]['low'],
+        'Close2': df.iloc[-1]['close']
     }
     data = {
-        'Date': df.tail(2)['datetime'].iloc[1],
-        'Open': df.tail(2)['open'].iloc[1],
-        'High': df.tail(2)['high'].iloc[1],
-        'Low': df.tail(2)['low'].iloc[1],
-        'Close': df.tail(2)['close'].iloc[1],
+        'Date': df.iloc[-1]['datetime'],
+        'Open': df.iloc[-1]['open'],
+        'High': df.iloc[-1]['high'],
+        'Low': df.iloc[-1]['low'],
+        'Close': df.iloc[-1]['close'],
         'Return': (data2['Close2']-data1['Close1'])/data1['Close1'],
         'Open_Close': data2['Close2']-data2['Open2'],
         'High_Low': data2['High2'] - data2['Low2'] if data2['Close2'] >= data2['Open2'] else data2['Low2'] - data2['High2']
@@ -47,7 +47,7 @@ if st.button("predict"):
    
     st.write(data)
     st.write(df.tail(2))
-    #st.write(prediction)   
+    st.write(prediction)   
 
         
 
