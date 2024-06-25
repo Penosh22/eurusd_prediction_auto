238d933c4b48de10d1528f03c922720f6afbfb56
Penosh22 <142080192+Penosh22@users.noreply.github.com>
2024-06-25 09:59:50 +0530

    Update deploy_app.py

--- deploy_app.py
+++ deploy_app.py
@@ -77,3 +77,17 @@ if st.button("predict"):
     else:
         print(f'Failed to retrieve the webpage. Status code: {response.status_code}')
         
+st.session_state['answer'] = ''!
+
+st.write(st.session_state)
+
+realans = ['', 'abc', 'edf']
+
+if  st.session_state['answer'] in realans:
+    answerStat = "correct"
+elif st.session_state['answer'] not in realans:
+    answerStat = "incorrect"
+
+st.write(st.session_state)
+st.write(answerStat)
+        
